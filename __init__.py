c66c1b50d5d15437a65412c45dac34701c069904
Josh Feldman <feldmanj@gmail.com>
2019-08-16 17:38:41 -0400

    initial framework

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
+
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap.init_app(app)
+
+    return app

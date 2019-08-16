4432f8d2632e40d8c64ead6bd521e53012f548f2
Josh Feldman <feldmanj@gmail.com>
2019-08-16 19:29:38 -0400

    framework updates

--- __init__.py
+++ __init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_bootstrap import Bootstrap
 
 bootstrap = Bootstrap()
+main = Blueprint('main', __name__)
 
 
 def create_app():
     app = Flask(__name__)
     bootstrap.init_app(app)
 
+    app.register_blueprint(main)
+
     return app
+
+from . import views
--- views.py
+++ views.py
@@ -1,8 +1,8 @@
 from flask import render_template
 
-from ytdl import app
+from . import main
 
 
-@app.route('/', methods=['GET', 'POST'])
+@main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')

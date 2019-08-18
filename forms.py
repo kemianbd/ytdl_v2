fd06de3946c5c162d7120555bf078fac31f358d2
Josh Feldman <feldmanj@gmail.com>
2019-08-18 14:18:20 -0400

    finish download form

--- forms.py
+++ forms.py
@@ -5,6 +5,8 @@ from wtforms.validators import DataRequired
 
 class URLForm(FlaskForm):
     urls = wtforms.TextAreaField('Enter YouTube URLs', validators=[DataRequired()])
+    bit = wtforms.SelectField('Choose bitrate:', coerce=int,
+                              choices=[(192, '192kb'), (256, '256kb'), (320, '320kb')], default=256)
     vid = wtforms.BooleanField('Keep video?')
     file = wtforms.StringField('Save location', validators=[DataRequired()])
     run = wtforms.SubmitField('Run')
--- views.py
+++ views.py
@@ -2,12 +2,13 @@ from flask import render_template
 
 from . import main
 from .forms import URLForm
+# from download import dl_convert
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = URLForm()
     if form.validate_on_submit():
-        print(form.urls.data)
-        print(form.vid.data)
+        # download.dl_convert(form.urls.data, form.file.data, form.vid.data, int(form.bit.data))
+        print(form.bit.data)
     return render_template('index.html', form=form)
